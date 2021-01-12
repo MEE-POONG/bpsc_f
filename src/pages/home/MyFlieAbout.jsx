ab69577329fa6f5c28712d8207b6262e3f71e3f4
Warayut Taekrathok <warayut2555@gmail.com>
2021-01-12 14:01:48 +0700

    chun

--- src/pages/home/MyFlieAbout.jsx
+++ src/pages/home/MyFlieAbout.jsx
@@ -6,23 +6,19 @@ import SheetThree from './sheet/SheetThree'
 class MyFlieAbout extends Component {
   render() {
     return (
-      <Container fluid className="home-file-about" >
+      <Container fluid className="home-file-about">
         <Image className="file-about" src="/image/fileabout.svg" />
-        <Container fluid className="my-file while" >
+        <Container fluid className="my-file while">
           <div className="tab-script">
-            <Button variant="light">
-              our Innovation
-            </Button>
+            <Button variant="light">our Innovation</Button>
           </div>
           <SheetOne />
           <SheetTwo />
           <SheetThree />
         </Container>
-        <Container fluid className="my-file green" >
+        <Container fluid className="my-file green">
           <div className="tab-script">
-            <Button variant="light">
-              our Innovation
-            </Button>
+            <Button variant="light">our Mission</Button>
           </div>
           <SheetOne />
           <SheetTwo />
