ebb207712dfa09214fa2b5c4e85a6c6c69a0b2c6
Warayut Taekrathok <warayut2555@gmail.com>
2021-01-12 13:48:16 +0700

    chun

--- src/pages/home/Learning.jsx
+++ src/pages/home/Learning.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Container, Card, Row, Col } from 'react-bootstrap';
-
+import { Container, Card, Row, Col } from "react-bootstrap";
 
 class Abstain extends Component {
   render() {
@@ -11,23 +10,28 @@ class Abstain extends Component {
             <Col lg="3" md="6" mb="4" xs="1">
               <Card>
                 <Card.Body className="image">
-                  <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top" />
+                  <img
+                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    alt=""
+                    class="card-img-top"
+                  />
                 </Card.Body>
                 <Card.Body>
-                  <Card.Text>
-                    Sunset
-                    </Card.Text>
+                  <Card.Text>Sunset</Card.Text>
                   <Card.Text>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    </Card.Text>
+                  </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                   <Card.Text className="mr-auto">
                     <span>
                       <span>
-                        Read more {'>'}
+                        READ MORE
+                        <i style={{ fontSize: "14px" }} class="fa pl-2">
+                          &#xf061;
+                        </i>
                       </span>
-                      {'<>'}
+                      {"<>"}
                     </span>
                   </Card.Text>
                 </Card.Footer>
@@ -36,23 +40,28 @@ class Abstain extends Component {
             <Col lg="3" md="6" mb="4" xs="1">
               <Card>
                 <Card.Body className="image">
-                  <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top" />
+                  <img
+                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    alt=""
+                    class="card-img-top"
+                  />
                 </Card.Body>
                 <Card.Body>
-                  <Card.Text>
-                    Sunset
-                    </Card.Text>
+                  <Card.Text>Sunset</Card.Text>
                   <Card.Text>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    </Card.Text>
+                  </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                   <Card.Text className="mr-auto">
                     <span>
                       <span>
-                        Read more {'>'}
+                        READ MORE
+                        <i style={{ fontSize: "14px" }} class="fa pl-2">
+                          &#xf061;
+                        </i>
                       </span>
-                      {'<>'}
+                      {"<>"}
                     </span>
                   </Card.Text>
                 </Card.Footer>
@@ -61,23 +70,28 @@ class Abstain extends Component {
             <Col lg="3" md="6" mb="4" xs="1">
               <Card>
                 <Card.Body className="image">
-                  <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top" />
+                  <img
+                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    alt=""
+                    class="card-img-top"
+                  />
                 </Card.Body>
                 <Card.Body>
-                  <Card.Text>
-                    Sunset
-                    </Card.Text>
+                  <Card.Text>Sunset</Card.Text>
                   <Card.Text>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    </Card.Text>
+                  </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                   <Card.Text className="mr-auto">
                     <span>
                       <span>
-                        Read more {'>'}
+                        READ MORE
+                        <i style={{ fontSize: "14px" }} class="fa pl-2">
+                          &#xf061;
+                        </i>
                       </span>
-                      {'<>'}
+                      {"<>"}
                     </span>
                   </Card.Text>
                 </Card.Footer>
@@ -86,23 +100,28 @@ class Abstain extends Component {
             <Col lg="3" md="6" mb="4" xs="1">
               <Card>
                 <Card.Body className="image">
-                  <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top" />
+                  <img
+                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    alt=""
+                    class="card-img-top"
+                  />
                 </Card.Body>
                 <Card.Body>
-                  <Card.Text>
-                    Sunset
-                    </Card.Text>
+                  <Card.Text>Sunset</Card.Text>
                   <Card.Text>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    </Card.Text>
+                  </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                   <Card.Text className="mr-auto">
                     <span>
                       <span>
-                        Read more {'>'}
+                        READ MORE
+                        <i style={{ fontSize: "14px" }} class="fa pl-2">
+                          &#xf061;
+                        </i>
                       </span>
-                      {'<>'}
+                      {"<>"}
                     </span>
                   </Card.Text>
                 </Card.Footer>
@@ -111,9 +130,8 @@ class Abstain extends Component {
           </Row>
         </Container>
       </div>
-
     );
   }
-};
+}
 
 export default Abstain;
