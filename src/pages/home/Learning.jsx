a7b8c6294ca97c75d279cd7031fd5ac9a34c6e89
Warayut Taekrathok <warayut2555@gmail.com>
2021-01-12 15:03:07 +0700

    chun

--- src/pages/home/Learning.jsx
+++ src/pages/home/Learning.jsx
@@ -11,7 +11,7 @@ class Abstain extends Component {
               <Card>
                 <Card.Body className="image">
                   <img
-                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    src="image/image12.png"
                     alt=""
                     class="card-img-top"
                   />
@@ -43,40 +43,7 @@ class Abstain extends Component {
               <Card>
                 <Card.Body className="image">
                   <img
-                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
-                    alt=""
-                    class="card-img-top"
-                  />
-                </Card.Body>
-                <Card.Body>
-                  <Card.Text>Sunset</Card.Text>
-                  <Card.Text>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Card.Text className="mr-auto">
-                    <span class="d-flex justify-content-between">
-                      <span style={{color: "#26BEB4"}}>
-                        READ MORE
-                        <i style={{ fontSize: "14px" }} class="fa pl-2">
-                          &#xf061;
-                        </i>
-                      </span>
-                      
-                      <span>
-                        <i class="fa fa-heart-o"></i> 112
-                      </span>
-                    </span>
-                  </Card.Text>
-                </Card.Footer>
-              </Card>
-            </Col>
-            <Col lg="3" md="6" mb="4" xs="1">
-              <Card>
-                <Card.Body className="image">
-                  <img
-                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    src="image/image11.png"
                     alt=""
                     class="card-img-top"
                   />
@@ -109,7 +76,40 @@ class Abstain extends Component {
               <Card>
                 <Card.Body className="image">
                   <img
-                    src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                    src="image/image10.png"
+                    alt=""
+                    class="card-img-top"
+                  />
+                </Card.Body>
+                <Card.Body>
+                  <Card.Text>Sunset</Card.Text>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Card.Text className="mr-auto">
+                    <span class="d-flex justify-content-between">
+                      <span style={{color: "#26BEB4"}}>
+                        READ MORE
+                        <i style={{ fontSize: "14px" }} class="fa pl-2">
+                          &#xf061;
+                        </i>
+                      </span>
+                      
+                      <span>
+                        <i class="fa fa-heart-o"></i> 112
+                      </span>
+                    </span>
+                  </Card.Text>
+                </Card.Footer>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" mb="4" xs="1">
+              <Card>
+                <Card.Body className="image">
+                  <img
+                    src="image/image6.png"
                     alt=""
                     class="card-img-top"
                   />
