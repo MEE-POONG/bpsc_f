731d263c2190e7c48ca5fb4c7f2e0637437444f8
Warayut Taekrathok <warayut2555@gmail.com>
2021-01-12 14:59:34 +0700

    chun

--- src/pages/home/sheet/SheetOne.jsx
+++ src/pages/home/sheet/SheetOne.jsx
@@ -1,7 +1,5 @@
-
-
 import React, { Component } from "react";
-import { Row, Col, Card, Media, ListGroup } from 'react-bootstrap';
+import { Row, Col, Card, Media, ListGroup, Image } from "react-bootstrap";
 
 class SheetOne extends Component {
   render() {
@@ -16,45 +14,85 @@ class SheetOne extends Component {
                 <div>IN BPSC</div>
               </Media.Body>
             </Media>
-            <div className="subtitle">
-              Short describtion about innovation
-              </div>
-            <div className="subtitle">
-              ......
-              </div>
+            <div className="subtitle">SHORT DESCRIPTION ABOUT INNOVATION</div>
+            <div className="subtitle">......</div>
           </Col>
           <Col lg="6" className="pr-0 m-0">
             <Row className="p-0 m-0">
-              <Col lg="6" >
+              <Col lg="6">
                 <ListGroup horizontal>
-                  <ListGroup.Item className="start">
-                    {/* <Image src="/image/myfileabout.svg"/> */}
+                  <ListGroup.Item className="start"style={{ backgroundColor: '#E5E5E5'}}>
+                    <center class="align-self-center pt-3">
+                      <Image src="/image/advice1.png" width="60px" />
+                      <br />
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        BRIEF ADVICE
+                      </p>
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        (BA)
+                      </p>
+                    </center>
                   </ListGroup.Item>
-                  <ListGroup.Item className="end">Short describtion about innovation!</ListGroup.Item>
+                  <ListGroup.Item className="end" style={{ fontSize: "12px" }}>
+                    SHORT DESCRIPTION ABOUT INNOVATION!
+                  </ListGroup.Item>
                 </ListGroup>
               </Col>
-              <Col lg="6" >
+              <Col lg="6">
                 <ListGroup horizontal>
-                  <ListGroup.Item className="start">
-                    {/* <Image src="/image/myfileabout.svg"/> */}
+                  <ListGroup.Item className="start" style={{ backgroundColor: '#E5E5E5'}}>
+                    <center class="align-self-center pt-3">
+                      <Image src="/image/together1.png" width="60px" />
+                      <br />
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        BRIEF ADVICE
+                      </p>
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        (BA)
+                      </p>
+                    </center>
                   </ListGroup.Item>
-                  <ListGroup.Item className="end">Short describtion about innovation!</ListGroup.Item>
+                  <ListGroup.Item className="end" style={{ fontSize: "12px" }}>
+                    SHORT DESCRIPTION ABOUT INNOVATION!
+                  </ListGroup.Item>
                 </ListGroup>
               </Col>
-              <Col lg="6" >
+              <Col lg="6">
                 <ListGroup horizontal>
-                  <ListGroup.Item className="start">
-                    {/* <Image src="/image/myfileabout.svg"/> */}
+                  <ListGroup.Item className="start"style={{ backgroundColor: '#E5E5E5'}}>
+                    <center class="align-self-center pt-3">
+                      <Image src="/image/patches1.png" width="60px" />
+                      <br />
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        BRIEF ADVICE
+                      </p>
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        (BA)
+                      </p>
+                    </center>
                   </ListGroup.Item>
-                  <ListGroup.Item className="end">Short describtion about innovation!</ListGroup.Item>
+                  <ListGroup.Item className="end" style={{ fontSize: "12px" }}>
+                    SHORT DESCRIPTION ABOUT INNOVATION!
+                  </ListGroup.Item>
                 </ListGroup>
               </Col>
-              <Col lg="6" >
+              <Col lg="6">
                 <ListGroup horizontal>
-                  <ListGroup.Item className="start">
-                    {/* <Image src="/image/myfileabout.svg"/> */}
+                  <ListGroup.Item className="start"style={{ backgroundColor: '#E5E5E5'}}>
+                    <center class="align-self-center pt-3">
+                      <Image src="/image/medical-report1.png" width="60px" />
+                      <br />
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        BRIEF ADVICE
+                      </p>
+                      <p style={{ fontSize: "x-small", margin: "unset" }}>
+                        (BA)
+                      </p>
+                    </center>
                   </ListGroup.Item>
-                  <ListGroup.Item className="end">Short describtion about innovation!</ListGroup.Item>
+                  <ListGroup.Item className="end" style={{ fontSize: "12px" }}>
+                    SHORT DESCRIPTION ABOUT INNOVATION!
+                  </ListGroup.Item>
                 </ListGroup>
               </Col>
             </Row>
@@ -63,5 +101,5 @@ class SheetOne extends Component {
       </div>
     );
   }
-};
+}
 export default SheetOne;
