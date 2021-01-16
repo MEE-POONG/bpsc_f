3061c76774751505175dd8a7931e06b57f955925
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-01-17 04:57:52 +0700

    est

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ export default function App() {
 
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="*" element={<FourZeroFour />} />
           </Routes>
