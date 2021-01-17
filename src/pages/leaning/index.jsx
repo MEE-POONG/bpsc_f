5e5d999f2316d032cb6b2d0d7a711eeef4b6a71d
warayut.ta <warayut.ta@princhealth.com>
2021-01-18 02:06:05 +0700

    เรียนรู้ผ่านระบบออนไลน์

---
+++ src/pages/leaning/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import LeaningDetail from "./LeaningDetail";
+import LeaningTitle from "./LeaningTitle";
+class Leaning extends Component {
+  render() {
+    return (
+      <div className="leaning-page text-uppercase">
+        <LeaningTitle/>
+        <LeaningDetail/>
+      </div>
+    );
+  }
+}
+
+export default Leaning;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const Home = React.lazy(() => import("./pages/home/Home"));
 const About = React.lazy(() => import("./pages/about"));
 const Sharing = React.lazy(() => import("./pages/sharing"));
 const Profile = React.lazy(() => import("./pages/profile"));
+const Leaning = React.lazy(() => import("./pages/leaning"));
 
 const FourZeroFour = React.lazy(() => import("./pages/404"));
 
@@ -13,6 +14,7 @@ const routes = [
   { path: "/about", name: "ABOUT US", component: About },
   { path: "/sharing", name: "SHARING", component: Sharing },
   { path: "/profile", name: "PROFILE", component: Profile },
+  { path: "/e-leaning", name: "LEANING", component: Leaning },
   { path: "*", name: "404", component: FourZeroFour }
 ];
 
