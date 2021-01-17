2636c7e19199a029d3fb0af22dcdbb661e508157
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-01-18 02:27:06 +0700

    Update TheMotivational.jsx

--- src/controllers/TheMotivational.jsx
+++ src/controllers/TheMotivational.jsx
@@ -8,7 +8,6 @@ const TheMotivational = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return (
-<<<<<<< HEAD
         <>
             <Button variant="primary" onClick={handleShow}>
                 Launch static backdrop modal
@@ -223,50 +222,6 @@ const TheMotivational = () => {
                 </Modal.Footer>
             </Modal>
         </>
-=======
-      <>
-        <Navbar
-          variant="light"
-          className="container-xl"
-          style={{ boxShadow: "none" }}
-          expand="lg"
-        >
-          {/* <Navbar bg="light" variant="light"> */}
-          <Navbar.Brand href="/">
-            <Image
-              alt=""
-              src="image/header/Logo.png"
-              width="100"
-              className="d-inline-block align-tops"
-              style={{ objectFit: "contain" }}
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-center">
-              <NavLink to="/" className="nav-link">
-                HOME
-              </NavLink>
-              <NavLink to="/about" className="nav-link">
-                ABOUT US
-              </NavLink>
-              <NavLink to="/sharing" className="nav-link">
-                SHARING
-              </NavLink>
-              <NavLink to="/e-leaning" className="nav-link">
-                E-LEARNING
-              </NavLink>
-              <NavLink to="/doctor" className="nav-link">
-                DOCTORS
-              </NavLink>
-              <Button variant="success" className="nav-link">
-                LOGIN
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </>
->>>>>>> 8be09219e51a0358ba18751fc39a076c9001a0ed
     );
 }
 export default TheMotivational;
