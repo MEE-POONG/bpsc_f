e4ec833e81282d82635977d46984a83852fbeeba
warayut.ta <warayut.ta@princhealth.com>
2021-01-25 23:04:35 +0700

    Update ProfileTitle.jsx

--- src/pages/profile/ProfileTitle.jsx
+++ src/pages/profile/ProfileTitle.jsx
@@ -1,16 +1,20 @@
+import React, {useState} from "react";
 import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import React from "react";
 import {Button, Container, Image} from "react-bootstrap";
 
 const ProfileTitle = () => {
+  const [username] = useState(
+    localStorage.getItem("firstName") + " " + localStorage.getItem("lastName")
+  );
+  const [email] = useState(localStorage.getItem("email"));
   return (
     <div className="profile-title text-uppercase">
       <div className="profile-title-bg scroll-num py-5">
         <Container className="text-center">
           <Image src="/image/image 19.png" className="about-team-circle" />
-          <p className="h1 pt-4">USERNAME</p>
-          <p className="h1 pt-2">E-MAIL</p>
+          <p className="h1 pt-4">{username}</p>
+          <p className="h1 pt-2">{email}</p>
           <div className="pt-2">
             <Button variant="light" className="text-dark">
               EDIT <FontAwesomeIcon icon={faEdit} />
