f2ecdb35aef2b1100964dc9dab97f1590b0ebfe7
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-02-11 00:05:35 +0700

    est

--- src/pages/about/AboutTeam.jsx
+++ src/pages/about/AboutTeam.jsx
@@ -7,24 +7,24 @@ const AboutTeam = () => {
       <div className="scroll-num py-5 about-team-bg">
         <Container className="text-center">
           <p className="h1 font-weight-bold">the team</p>
-          <Row>
-            <Col className="pt-5">
+          <Row className="justify-content-center">
+            <Col lg="4" className="pt-5">
               <Image src="/image/image45.png" className="about-team-circle" />
               <p className="h4 font-weight-bold pt-3">Name Surname Admin</p>
             </Col>
-            <Col className="pt-5">
+            <Col lg="4" className="pt-5">
               <Image src="/image/image 46.png" className="about-team-circle" />
               <p className="h4 font-weight-bold pt-3">Name Surname Admin</p>
             </Col>
-            <Col className="pt-5">
+            <Col lg="4" className="pt-5">
               <Image src="/image/image 47.png" className="about-team-circle" />
               <p className="h4 font-weight-bold pt-3">Name Surname Admin</p>
             </Col>
-            <Col className="pt-5">
+            <Col lg="4" className="pt-5">
               <Image src="/image/image 48.png" className="about-team-circle" />
               <p className="h4 font-weight-bold pt-3">Name Surname Admin</p>
             </Col>
-            <Col className="pt-5">
+            <Col lg="4" className="pt-5">
               <Image src="/image/image 49.png" className="about-team-circle" />
               <p className="h4 font-weight-bold pt-3">Name Surname Admin</p>
             </Col>
