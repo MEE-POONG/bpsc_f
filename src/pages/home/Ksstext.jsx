b614a30c3203ade3437a54491d9ae4ca59022278
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-02-14 00:45:45 +0700

    est

--- src/pages/home/Ksstext.jsx
+++ src/pages/home/Ksstext.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Card, Container,Row, Col, Image } from "react-bootstrap";
+import { Card, Container,Row, Col, Image, Button } from "react-bootstrap";
 const Ksstext = () => {
   return (
     <div className="ksstext">
       <Container className="py-5">
         <Row>
-          <Col className="text-right">
+          <Col md="6" className="img">
             <Image src="/image/home/image4.png" />
           </Col>
-          <Col>
+          <Col md="6">
             <Card.Title>
               กสศ
             </Card.Title>
             <Card.Subtitle>
               insKru ร่วมเป็นส่วนหนึ่งในการขับเคลื่อน "ห้องเรียนเสมอภาค" ร่วมกับกองทุนเพื่อความเสมอภาคทางการศึกษา
             </Card.Subtitle>
+            <Button bsPrefix="kss">ไปเว็บ กสศ กันเลย!</Button>
           </Col>
         </Row>
       </Container>
