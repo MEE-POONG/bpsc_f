80ae588a6c51ecc754b57423e26a5511c9cecce5
devilzeros <42342644+devilzeros@users.noreply.github.com>
2021-02-15 22:20:44 +0700

    est

--- src/pages/home/EventBox.jsx
+++ src/pages/home/EventBox.jsx
@@ -1,32 +1,16 @@
-import moment from "moment";
 import React, { useState, useEffect } from "react";
-import { Container, Card, Modal, Row, Col, Media } from "react-bootstrap";
-import { faClock, faEye, faMapMarkerAlt, faLocationArrow, faMapMarked } from "@fortawesome/free-solid-svg-icons";
+import { Button, Container, Card, Modal, Row, Col, Media } from "react-bootstrap";
+import { faClock, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/swiper.scss";
-
-import { API_GET_EVENT } from "../../apis";
 
 const EventBox = () => {
-  const [show, setShow] = useState(false);
-  const [showData, setShowData] = useState(0);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const handleShowData = (id) => setShowData(id);
-  const [event, setEvent] = useState(null);
-
-  useEffect(() => {
-    API_GET_EVENT().then((result) => {
-      setEvent(result?.data);
-    });
-  }, []);
+  const [showEventAll, setShowEventAll] = useState(false);
+  const handleClose = () => setShowEventAll(false);
+  const handleShow = () => setShowEventAll(true);
 
   return (
     <div className="home-event text-uppercase">
-      <Container className="pt-5">
+      <Container className="py-5">
         <Row>
           <Col lg="6" md="6" sm="6" xs="12">
             <Card.Title>Up coming events</Card.Title>
@@ -35,15 +19,65 @@ const EventBox = () => {
             {/* เว้นไว้ */}
           </Col>
           <Col lg="6" md="6" sm="6" xs="12">
-            {/* เว้นไว้ */}
+            <Card className="view frist">
+              <Row className="m-0">
+                <Col xs="auto" lg="auto" className="date p-0">
+                  <div className="start">
+                    <Card.Title>
+                      19
+                    </Card.Title>
+                    <Card.Subtitle>
+                      FEB
+                    </Card.Subtitle>
+                  </div>
+                  <div className="end">
+                    <Card.Title>
+                      2
+                    </Card.Title>
+                    <Card.Subtitle>
+                      MAR
+                    </Card.Subtitle>
+                  </div>
+                </Col>
+                <Col>
+                  <Row>
+                    <Col lg="12" className="d-flex">
+                      <Media className="location align-self-center">
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faMapMarkerAlt} />
+                        <Media.Body>
+                          <div>Room 257,  building name BAngkapi bangkok</div>
+                        </Media.Body>
+                      </Media>
+                    </Col>
+                    <Col lg="12" className="detail" >
+                      <Media className="date-time align-self-center">
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faClock} />
+                        <Media.Body>
+                          <div>10.30 AM - 12.00 PM</div>
+                          <div></div>
+                        </Media.Body>
+                      </Media>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col lg="12" xs="12" className="f-detail">
+                  <Card.Title>
+                    ProgrAm NAME
+                      </Card.Title>
+                  <Card.Subtitle>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do  ...
+                      </Card.Subtitle>
+                </Col>
+              </Row>
+            </Card>
           </Col>
           <Col lg="6" md="6" sm="6" xs="12" style={{ position: "relative" }}>
-            <Card.Subtitle bsPrefix="view-all" style={{
+            <Card.Subtitle bsPrefix="view-all" onClick={handleShow} style={{
               position: "absolute",
               right: "2rem",
               bottom: "100%"
             }}>View all Events</Card.Subtitle>
-            <Card className="view my-3">
+            <Card className="view start">
               <Row className="m-0">
                 <Col xs="auto" lg="auto" className="date p-0">
                   <div className="start alone">
@@ -67,18 +101,18 @@ const EventBox = () => {
                   <Row>
                     <Col lg="6" className="d-flex">
                       <Media className="location align-self-center">
-                        <FontAwesomeIcon className="mr-2 align-self-center" icon={faMapMarkerAlt} />
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faMapMarkerAlt} />
                         <Media.Body>
-                          <span> building name</span>
+                          <div> building name</div>
                         </Media.Body>
                       </Media>
                     </Col>
                     <Col lg="6" >
                       <Media className="date-time align-self-center">
-                        <FontAwesomeIcon className="mr-2 align-self-center" icon={faClock} />
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faClock} />
                         <Media.Body>
-                          <span>10.30 AM - 12.00 PM</span>
-                          <span>2.00 PM - 3.30 PM</span>
+                          <div>10.30 AM - 12.00 PM</div>
+                          <div>2.00 PM - 3.30 PM</div>
                         </Media.Body>
                       </Media>
                     </Col>
@@ -94,7 +128,7 @@ const EventBox = () => {
                 </Col>
               </Row>
             </Card>
-            <Card className="view my-3">
+            <Card className="view end">
               <Row className="m-0">
                 <Col xs="auto" lg="auto" className="date p-0">
                   <div className="start">
@@ -118,18 +152,18 @@ const EventBox = () => {
                   <Row>
                     <Col lg="6" className="d-flex">
                       <Media className="location align-self-center">
-                        <FontAwesomeIcon className="mr-2 align-self-center" icon={faMapMarkerAlt} />
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faMapMarkerAlt} />
                         <Media.Body>
-                          <span> building name</span>
+                          <div> building name</div>
                         </Media.Body>
                       </Media>
                     </Col>
                     <Col lg="6" >
                       <Media className="date-time align-self-center">
-                        <FontAwesomeIcon className="mr-2 align-self-center" icon={faClock} />
+                        <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faClock} />
                         <Media.Body>
-                          <span>10.30 AM - 12.00 PM</span>
-                          <span></span>
+                          <div>10.30 AM - 12.00 PM</div>
+                          <div></div>
                         </Media.Body>
                       </Media>
                     </Col>
@@ -148,14 +182,137 @@ const EventBox = () => {
           </Col>
         </Row>
       </Container>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton></Modal.Header>
+      <Modal
+        show={showEventAll}
+        onHide={handleClose}
+        // backdrop="static"
+        // keyboard={false}
+        centered
+        size="lg"
+        className="home-event-list"
+      >
+        <Modal.Header>
+          <Modal.Title bsPrefix="f-gradient">EVENT</Modal.Title>
+        </Modal.Header>
         <Modal.Body>
-          <Card.Body>
-            <Card.Title><h2>{event?.data[showData].title}</h2></Card.Title>
-            <Card.Subtitle><h3>{event?.data[showData].content}</h3></Card.Subtitle>
-          </Card.Body>
+          <Row className="mx-2">
+            <Col xs="6" lg="6">
+              <Card className="view start">
+                <Row className="m-0 meat-blue">
+                  <Col xs="auto" lg="auto" className="date p-0">
+                    <div className="start alone">
+                      <Card.Title>
+                        19
+                    </Card.Title>
+                      <Card.Subtitle>
+                        FEB
+                    </Card.Subtitle>
+                    </div>
+                    <div className="end d-none">
+                      <Card.Title>
+                        2
+                    </Card.Title>
+                      <Card.Subtitle>
+                        MAR
+                    </Card.Subtitle>
+                    </div>
+                  </Col>
+                  <Col>
+                    <Row>
+                      <Col lg="12" className="d-flex">
+                        <Media className="location align-self-center">
+                          <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faMapMarkerAlt} />
+                          <Media.Body>
+                            <div> building name</div>
+                          </Media.Body>
+                        </Media>
+                      </Col>
+                      <Col lg="12" className="f-detail">
+                        <Media className="date-time align-self-center">
+                          <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faClock} />
+                          <Media.Body>
+                            <div>10.30 AM - 12.00 PM</div>
+                            <div>2.00 PM - 3.30 PM</div>
+                          </Media.Body>
+                        </Media>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg="12" xs="12" className="detail set-margin">
+                    <Card.Title>
+                      ProgrAm NAME
+                      </Card.Title>
+                    <Card.Subtitle>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do  ...
+                      </Card.Subtitle>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+            <Col xs="6" lg="6">
+              <Card className="view">
+                <Row className="m-0 meat-blue">
+                  <Col xs="auto" lg="auto" className="date p-0">
+                    <div className="start">
+                      <Card.Title>
+                        19
+                    </Card.Title>
+                      <Card.Subtitle>
+                        FEB
+                    </Card.Subtitle>
+                    </div>
+                    <div className="end">
+                      <Card.Title>
+                        2
+                    </Card.Title>
+                      <Card.Subtitle>
+                        MAR
+                    </Card.Subtitle>
+                    </div>
+                  </Col>
+                  <Col>
+                    <Row>
+                      <Col lg="12" className="d-flex">
+                        <Media className="location align-self-center">
+                          <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faMapMarkerAlt} />
+                          <Media.Body>
+                            <div> building name</div>
+                          </Media.Body>
+                        </Media>
+                      </Col>
+                      <Col lg="12" className="f-detail">
+                        <Media className="date-time align-self-center">
+                          <FontAwesomeIcon className="mr-2 fas align-self-center" icon={faClock} />
+                          <Media.Body>
+                            <div>10.30 AM - 12.00 PM</div>
+                            <div></div>
+                          </Media.Body>
+                        </Media>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg="12" xs="12" className="detail set-margin" >
+                    <Card.Title>
+                      ProgrAm NAME
+                      </Card.Title>
+                    <Card.Subtitle>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do  ...
+                      </Card.Subtitle>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
         </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            BACK
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
